3312626c42cb9d07d74dc6083dd3ea0fb5f4a785
nikju <nikjust08@mail.com>
2022-05-10 16:50:46 +0500

    Any aspect ratio update!

--- docs/script.js
+++ docs/script.js
@@ -6,25 +6,26 @@ function randomIntFromInterval(min, max, random) { // min and max included
     return Math.floor(random.quick() * (max - min + 1) + min)
 }
 
-function drawAvatar(seed, pixelsize, pixelcount) {
+function drawAvatar(seed, pixelsize, pixelcountx, pixelcounty) {
     myrng = new Math.seedrandom(seed);
-    let canvassize = pixelsize*pixelcount;
+    let canvassizex = pixelsize*pixelcountx;
+    let canvassizey = pixelsize*pixelcounty;
 
 
     let randomColor = Math.floor( randomIntFromInterval(10066329,16777215, myrng)).toString(16);
     let canvas = document.getElementById("canvas");
     let ctx = canvas.getContext('2d');
-    canvas.width = canvassize;
-    canvas.height = canvassize;
+    canvas.width = canvassizex;
+    canvas.height = canvassizey;
 
-    for (let f = 0; f < pixelcount; f++) {
-        for (let i = 0; i < pixelcount/2+1; i++) {
+    for (let f = 0; f < pixelcounty; f++) {
+        for (let i = 0; i < pixelcountx/2+1; i++) {
             ctx.fillStyle = '#fff';
             if (getRandomInt(2, myrng) == 1) {
                 ctx.fillStyle = '#' + randomColor;
             }
             ctx.fillRect(i * pixelsize - pixelsize, f*pixelsize, pixelsize, pixelsize);
-            ctx.fillRect(canvassize - i * pixelsize, f*pixelsize, pixelsize, pixelsize);
+            ctx.fillRect(canvassizex - i * pixelsize, f*pixelsize, pixelsize, pixelsize);
         }
     }
 }
@@ -32,6 +33,7 @@ function drawAvatar(seed, pixelsize, pixelcount) {
 function onButtonClick() {
     let seed = document.getElementById("input").value;
     let pxsize = document.getElementById("pixelsize").value;
-    let pxcount = document.getElementById("pixels").value;
-    drawAvatar(seed, pxsize, pxcount);
+    let pxcounty = document.getElementById("pixelsy").value;
+    let pxcountx = document.getElementById("pixelsx").value;
+    drawAvatar(seed, pxsize, pxcountx, pxcounty);
 }
